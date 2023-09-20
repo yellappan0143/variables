f2219aa447b4921ff87cd6d7ef6a6fe5e5a8697d
YELLAPPAN P <109733281+Madesh0143@users.noreply.github.com>
2023-09-20 21:53:48 +0530

    Create M_L_variable_1.java

---
+++ M_L_variable_1.java
@@ -0,0 +1,28 @@
+//1. IF DATA MEMBER AND FUNCTION MEMBER ASSIGNED BY "STATIC" AND "STATIC" ONLY ACCESING MEMBERNAME
+
+package OOPS;
+
+public class M_L_variable_1
+{
+    // DATA MEMBER IS STATIC
+    static int A = 500;        // STATIC PRIMITIVE VARIABLES
+    static String Sub;        // NON-STATIC REFERENCE VARIABLES
+
+    // FUNCTION MEMBER OR METHOD IS STATIC
+    public static void test() 
+    {
+        int A =100;             // NON-STATIC LOCAL PRIMITIVE VARIABLES
+        String Sub = "SQL";    // NON-STATIC LOCAL REFERENCE VARIABLES
+
+        System.out.println(A);
+        System.out.println(Sub);
+
+        System.out.println(M_L_variable.A);    // CALLING FROM DATA MEMBER
+        System.out.println(M_L_variable.Sub);  // CLASSNAME.MEMBERNAME  
+    }
+
+    public static void main(String[] args) 
+    {
+        test();
+    }
+}
